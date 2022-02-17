5a95dce34b319388a699e1f5b01eee86472a7129
dlwlrma123 <alexfinhart@gmail.com>
2022-02-17 22:10:35 +0800

    configs: add an option to choose where to copy apns-conf.xml * on some legacy devices copying apns-conf.xml to product causes this error:
    E/TelephonyProvider(2846): FileNotFoundException for /system/etc/apns-conf.xml:java.io.FileNotFoundException: /system/etc/apns-conf.xml: open failed: ENOENT (No such file or directory)
    
    * while also copying the apns-conf to system somehow breaks apn on some devices which causes missing default apn on some devices (issue reported after switching to system directory).
    
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- config/telephony.mk
+++ config/telephony.mk
@@ -2,9 +2,15 @@
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/etc/sensitive_pn.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sensitive_pn.xml
 
+ifeq ($(COPY_APN_SYSTEM),true)
 # World APN list
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/etc/apns-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/apns-conf.xml
+else
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/etc/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+endif
 
 # Telephony packages
 PRODUCT_PACKAGES += \

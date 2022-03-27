3746acc4ce192794f5e387f187d2e14506e4b0eb
dlwlrma123 <alexfinhart@gmail.com>
2022-03-27 16:19:57 +0800

    arcana: version 6.0 "I am unfamiliar with this feeling. Is it joy?" - faerie dragon
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#Arcana Versioning
-ARCANA_VERSION = EVERGLOW
-ARCANA_CODE = 5.1
+#Arcana Version
+ARCANA_VERSION = FAERIE
+ARCANA_CODE = 6.0
 
 ifeq ($(WITH_GAPPS),true)
 PACKAGE_BUILD_TYPE ?= PIXEL
